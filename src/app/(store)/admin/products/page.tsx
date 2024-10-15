76aaae26ba70029a1d745e3012433568b68b3cfa
willian <willian.araujo1994@gmail.com>
2024-10-15 15:29:25 -0300

    feat: added product post request and validations

--- src/app/(store)/admin/products/page.tsx
+++ src/app/(store)/admin/products/page.tsx
@@ -1,26 +1,28 @@
 'use client';
 import { Button } from "@/components/ui/button";
-import { Product, columns } from "./columns";
+import { columns } from "./columns";
 import { DataTable } from './data-table';
-import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+import { Dialog, DialogClose, DialogContent, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { useEffect, useRef, useState } from "react";
 import { DialogDescription } from "@radix-ui/react-dialog";
 import { Textarea } from "@/components/ui/textarea";
 import { ImageUp } from "lucide-react";
- 
+import { Category } from "@/models/category.model";
+import { Toaster } from "@/components/ui/toaster"
+import { Product } from "@/models/product.model";
+import { z } from "zod";
+import { useToast } from "@/hooks/use-toast"
+
 async function getData(): Promise<Product[]> {
   const response = await fetch('http://localhost:8080/products');
+  return response.json();
+}
+
+async function getCategoryOptions(): Promise<Category[]> {
+  const response = await fetch('http://localhost:8080/categories');
   return response.json();
 }
 
@@ -29,15 +31,36 @@ function formatToBRL(value: string) {
   return numericValue.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
 }
 
+const formSchema = z.object({
+  name: z.string().min(3, { message: 'O nome deve conter no mínimo 3 caracteres' }),
+  price: z.number().min(0.01, { message: 'O preço deve ser maior que 0' }),
+  description: z.string().min(3, { message: 'A descrição deve conter no mínimo 3 caracteres' }),
+  category: z.string().min(1, { message: 'Selecione uma categoria' }),
+  image: z.string(),
+})
+
 export default function ProductsPage() {
   const [data, setData] = useState<Product[]>([]);
+  const [name, setName] = useState("");
   const [price, setPrice] = useState("R$ 0,00");
+  const [selectedCategory, setSelectedCategory] = useState<Category | null>(null);
+  const [description, setDescription] = useState("");
+  const [categories, setCategories] = useState<Category[]>([]);
+  const [errors, setErrors] = useState<{ [key: string]: string[] }>({});
+  const { toast } = useToast();
+  const [open, setOpen] = useState(false);
+
 
   const handlePriceChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const rawValue = e.target.value;
     const formattedValue = formatToBRL(rawValue);
     setPrice(formattedValue);
   };
+
+  const handleCategoryChange = (value: string) => {
+    const category = categories.find((category) => category.id === value);
+    setSelectedCategory(category || null);
+  }
 
   const fileInputRef = useRef<HTMLInputElement>(null);
 
@@ -53,19 +76,69 @@ export default function ProductsPage() {
     }
   };
 
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const productData = {
+      name,
+      price: parseFloat(price.replace(/[^0-9]/g, '')) / 100,
+      description,
+      category: selectedCategory?.name,
+      image: "nome-da-imagem.png",
+    };
+
+    const validation = formSchema.safeParse(productData);
+    if (!validation.success) {
+      const newErrors = validation.error.flatten().fieldErrors;
+      setErrors(newErrors);
+      toast({ description: "Por favor, corrija os erros antes de continuar." });
+      return;
+    }
+
+    errors && toast({
+      description: "Por favor, corrija os erros antes de continuar.",
+      variant: "destructive",
+      title: "Erro ao cadastrar produto."
+    });
+
+    try {
+      const response = await fetch('http://localhost:8080/products', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(productData),
+      });
+      if (!response.ok) {
+        throw new Error('Erro ao cadastrar produto.');
+      }
+      setErrors({});
+      toast({ description: "Produto cadastrado com sucesso!" });
+      setOpen(false);
+      
+    } catch (error) {
+      toast({ description: "Erro ao cadastrar produto." });
+    }
+  }
+
   useEffect(() => {
     async function fetchData() {
       const result = await getData();
       setData(result);
     }
+
+    async function fetchCategories() {
+      const result = await getCategoryOptions();
+      setCategories(result);
+    }
     fetchData();
+    fetchCategories();
   }, []);
 
   return (
     <main className='flex flex-col mx-auto lg:w-[1024px] xl:w-[1280px] 2xl:w-[1440px] px-10 my-4'>
       <h1 className='text-2xl font-bold'>Produtos</h1>
       <div className='flex gap-4 justify-end'>
-        <Dialog>
+        <Dialog open={open} onOpenChange={setOpen}>
           <DialogTrigger asChild>
             <Button variant="default" className="px-10">Cadastrar</Button>
           </DialogTrigger>
@@ -74,67 +147,83 @@ export default function ProductsPage() {
               <DialogTitle>Cadastrar produto</DialogTitle>
               <DialogDescription className="text-sm">Preencha os campos abaixo para cadastrar um novo produto.</DialogDescription>
             </DialogHeader>
-            <div className="flex items-center space-x-2">
-              <div className="grid flex-1 gap-2">
-                <Label htmlFor="name">
-                  Nome
-                </Label>
-                <Input
-                  id="name"
-                />
-              </div>
-            </div>
-            <div className="flex gap-4 items-end">
-              <Select>
-                <SelectTrigger>
-                  <SelectValue placeholder="Categoria" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="1">Categoria 1</SelectItem>
-                  <SelectItem value="2">Categoria 2</SelectItem>
-                  <SelectItem value="3">Categoria 3</SelectItem>
-                </SelectContent>
-              </Select>
-              <div>
-
-              <Label htmlFor="price" className="">
-                Preço
-              </Label>
-              <Input
-                id="price"
-                type="text"
-                placeholder="Preço"
-                value={price}
-                onChange={handlePriceChange}
-                />
+            <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+              <div className="flex items-center space-x-2">
+                <div className="grid flex-1 gap-2">
+                  <Label htmlFor="name" className={errors.name ? 'text-red-500' : ''}>
+                    Nome
+                  </Label>
+                  <Input
+                    id="name"
+                    type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    className={errors.name ? 'border-red-500' : ''}
+                  />
                 </div>
-            </div>
-            <Textarea placeholder="Descrição" minLength={3} maxLength={2500} className="min-h-32 max-h-52" />
-            <div className="flex gap-4 items-center">
-              <Input
-                id="image"
-                type="file"
-                ref={fileInputRef}
-                style={{ display: 'none' }}
-                onChange={handleFileChange}
+              </div>
+              <div className="flex gap-4 items-end">
+                <Select onValueChange={handleCategoryChange}>
+                  <SelectTrigger className={errors.category ? 'border-red-500' : ''}>
+                    <SelectValue placeholder="Categoria" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    {categories.map((category) => (
+                      <SelectItem key={category.id} value={category.id}>
+                        {category.name}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+                <div>
+                  <Label htmlFor="price" className={errors.price ? 'text-red-500' : ''}>
+                    Preço
+                  </Label>
+                  <Input
+                    id="price"
+                    type="text"
+                    placeholder="Preço"
+                    value={price}
+                    onChange={handlePriceChange}
+                    className={errors.price ? 'border-red-500' : ''}
+                  />
+                </div>
+              </div>
+              <Textarea
+                placeholder="Descrição"
+                minLength={3}
+                maxLength={2500}
+                className={`min-h-32 max-h-52 ${errors.description ? 'border-red-500' : ''}`}
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
               />
-              <Button onClick={handleButtonClick} className="flex gap-2 w-full text-center" variant="outline">
-                <ImageUp />
-                Escolher Imagem
-              </Button>
-            </div>
-            <DialogFooter className="justify-start md:justify-between">
-              <DialogClose asChild>
-                <Button type="reset" variant="secondary">
-                  Fechar
+              <div className="flex gap-4 items-center">
+                <Input
+                  id="image"
+                  type="file"
+                  ref={fileInputRef}
+                  style={{ display: 'none' }}
+                  onChange={handleFileChange}
+                />
+                <Button onClick={handleButtonClick} className={`flex gap-2 w-full text-center ${errors.image ? 'border-red-500' : ''} `} variant="outline" >
+                  <ImageUp />
+                  Escolher Imagem
                 </Button>
-              </DialogClose>
-              <Button type="submit">Cadastrar</Button>
-            </DialogFooter>
+              </div>
+              <DialogFooter className="justify-start md:justify-between">
+                <DialogClose asChild>
+                  <Button type="reset" variant="secondary">
+                    Fechar
+                  </Button>
+                </DialogClose>
+                <Button type="submit">Cadastrar</Button>
+              </DialogFooter>
+            </form>
           </DialogContent>
         </Dialog>
       </div>
       <DataTable columns={columns} data={data} />
+      <Toaster />
     </main>
   )
 }
